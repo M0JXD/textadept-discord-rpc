fd4133b0619a872bc7fb42498772742c312c04a9
M0JXD <jamiedrinkell@gmail.com>
2025-12-20 23:34:23 +0000

    Better startup and reconnect behaviour

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@
 M = {}
 M.show_connected = true  -- Update the statusbar
 M.private_mode = false  -- Be more vague with details, e.g. no file or folder names
+local max_tries = 10  -- Maximum tries at startup to to connect - TODO: Let's not change this variable
 local is_connected = false  -- To track whether we're connected
 
 local lib = 'discord_rpc.discordrpc'
@@ -29,8 +30,32 @@ M.presence = {
 	-- TODO: Add Party/Match/Secret and Buttons options?
 }
 
--- Convenience wrapper that will get current details before calling rpc.update() and update UI
-function M.update()
+local function attach_handlers()
+	-- Attach close handlers to shutdown Discord RPC cleanly
+	events.connect(events.QUIT, function ()
+		M.rpc.close()
+		return nil
+	end, 1)
+	events.connect(events.RESET_BEFORE, function ()
+		M.rpc.close() ; is_connected = false
+	end)
+
+	-- Attach updater
+	events.connect(events.UPDATE_UI, M.update)
+end
+
+local function remove_handlers()
+	events.disconnect(events.UPDATE_UI, M.update)
+	events.disconnect(events.RESET_BEFORE, function ()
+		M.rpc.close() ; is_connected = false
+	end)
+	events.disconnect(events.QUIT, function ()
+		M.rpc.close()
+		return nil
+	end, 1)  -- TODO: Is this right?
+end
+
+local function update_presence_details()
 	-- TODO: Handle edge cases like CMake etc.
 	local capitalised_type = buffer:get_lexer():sub(1,1):upper()..buffer:get_lexer():sub(2)
 
@@ -38,7 +63,7 @@ function M.update()
 	-- TODO: Details like running, editing, debugging etc.
 	local task = buffer.modify and 'editing ' or 'viewing '
 
-	local filestate = 'unknown'
+	local filestate = 'Untitled'
 	if (M.private_mode) then
 		filestate = 'a ' .. capitalised_type .. ' file.'
 	else
@@ -73,57 +98,76 @@ function M.update()
 	M.presence.largeImageText = 'Working on a ' .. capitalised_type ..
 		(capitalised_type:find('file') and '.' or ' file.')
 
-	-- Send away and get the stats
+end
+
+-- Convenience wrapper that will get current details before calling rpc.update() and update UI
+function M.update()
+	update_presence_details()
 	M.stats = M.rpc.update(M.presence)
 
 	-- Show the details
 	if (M.show_connected) then
-		if M.stats.errcode ~= 0 then
-			ui.statusbar_text = M.stats.errDetails
+		local spacing = CURSES and '  ' or '    '
+		if (M.stats.lastCallback == 0) then
+			-- Discord hasn't run any handlers yet, try sending another update
+			is_connected = false
+			if (max_tries ~= 0) then
+				max_tries = max_tries - 1
+				timeout(0.2, function ()
+					ui.statusbar_text = 'Attempting to connect to Discord...'
+					M.update()
+				end)
+			else
+				ui.statusbar_text = 'Could not connect to Discord.'
+				M.rpc.close()  -- Just in case it's some weird connection issue
+			end
+		elseif (M.stats.lastCallback == 1) then
+			if (is_connected == false) then
+				ui.statusbar_text = 'Connected to ' .. M.stats.username .. '.'
+				is_connected = true
+				attach_handlers()
+			end
+		elseif (M.stats.lastCallback == 2) then
+			ui.statusbar_text = 'Discord Disconnect: ' .. M.stats.errcode .. M.stats.errDetails
+			is_connected = false ; remove_handlers()
+			M.rpc.close()
+		elseif (M.stats.lastCallback == 3) then
+			ui.statusbar_text = 'Discord Error: ' .. M.stats.errcode .. M.stats.errDetails
+			is_connected = false ; remove_handlers()
+			M.rpc.close()
 		end
 
-		local spacing = CURSES and '  ' or '    '
-		local status = M.stats.username ~= '' and '☺' or '☹'
 		if (ui.buffer_statusbar_text:match('DRPC') == nil) then
-			ui.buffer_statusbar_text = ui.buffer_statusbar_text .. spacing .. 'DRPC: '.. status
+			ui.buffer_statusbar_text = ui.buffer_statusbar_text .. spacing .. 'DRPC: '.. (is_connected and '☺' or '☹')
+		else
+			local without_status = string.sub(ui.buffer_statusbar_text, 1, -4)
+			ui.buffer_statusbar_text = without_status .. (is_connected and '☺' or '☹')
 		end
 	end
 end
 
--- Convenience to allow user to 'start' RPC in their init.lua,
--- but actually starts RPC once Textadept is fully initialised
+
+-- Convenience to allow user to 'start' RPC in their init.lua
+-- Actually starts RPC once Textadept is fully initialised (so buffer/lexer names etc. will be present)
 function M.init()
 	events.connect(events.INITIALIZED, function ()
 		M.rpc.init()
 		M.update()
-
-		-- Attach close handlers to shutdown Discord RPC cleanly
-		events.connect(events.QUIT, function ()
-			M.rpc.close()
-			return nil
-		end, 1)
-		events.connect(events.RESET_BEFORE, M.rpc.close)
-
-		-- Attach updater
-		events.connect(events.UPDATE_UI, function (updated)
-			M.update()
-			-- Discord is a little slow to respond with it's connected status,
-			-- and without a good way of it emitting a Textadept event in the handler
-			-- This is the best idea I have :(
-			-- TODO: Put this in a coroutine loop so connected status can show by itself
-			if (M.stats.lastCallback == 1 and (is_connected == false)) then
-				ui.statusbar_text = 'Connected to ' .. M.stats.username .. '.'
-				is_connected = true
-			end
-		end)
 	end)
 end
 
 -- Mainly to allow reconnecting but can also be called to connect
+-- NOTE: The Discord RPC library has it's own retry mechanism that might mean this won't run for a while
 function M.reconnect()
-	M.rpc.close()
-	M.rpc.init()
-	M.update()
+	if (is_connected) then
+		M.rpc.close() ; is_connected = false
+		remove_handlers()
+	end
+
+	timeout(0.2, function ()
+		M.rpc.init()
+		M.update()
+	end)
 end
 
 -- TODO: Add a reconnect option under help menu
