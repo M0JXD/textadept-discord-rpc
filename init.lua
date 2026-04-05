97e7dd8585e7254c0c2dcfd2a2ff2e9875c22b54
M0JXD <jamiedrinkell@gmail.com>
2026-04-05 15:04:53 +0100

    Remove names and refactor
    - Quit and reset events did not need specialised before buffer close
    - Add 'an' or 'a' support
    - More detailed status dialog

--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
 -- Copyright 2025-2026 Jamie Drinkell. See LICENSE.
 -- Textadept Discord Rich Presence
 
-M = {mt = {}}
-M.show_connected = true -- Display 'DRPC' in buffer_statusbar
-M.private_mode = true -- Be more vague with details, e.g. no file or folder names
-M.connect_attempts = 10 -- Maximum tries at startup to to connect
-local last_action = 'confused at '
-local attempts = 0
-local is_connected = false -- To track whether we're connected
+local M = {mt = {}}
+M.show_connected = true -- Display 'DRPC' status in buffer_statusbar
+M.private = true -- Be more vague with details, e.g. no file or folder names
+M.attempts = 7 -- Maximum allowed attempts to connect
+
+local attempts = 0 -- Current attempt number
+local last_action = 'confused at ' -- Last build/run/test action
+local is_connected = false -- Are we connected to Discord?
+local handlers = false -- Are handlers connected?
+local old_lexer = 'Untitled' -- To track what the output buffer (probably) reflects
 
 local lib = 'discord_rpc.discordrpc'
 if OSX then
@@ -16,6 +19,7 @@ elseif LINUX and io.popen('uname -m'):read() == 'aarch64' then
 	lib = lib .. 'arm' -- TODO: Can Discord/ARMCord even support this?
 end
 M.rpc = require(lib)
+M.display = require('discord_rpc.names')
 
 -- LuaFormatter off
 M.stats = {
@@ -39,37 +43,6 @@ M.presence = {
 	largeImageKey = '',
 	largeImageText = ''
 	-- TODO: Add Party/Match/Secret and Buttons options?
-}
-
--- Lexer names that are not suitable for first letter capitalisation
-M.display_names = {
-	applescript = "AppleScript",
-	asm = "ASM",
-	asp = "ASP",
-	autoit = "AutoIt",
-	cmake = "CMake",
-	coffeescript = "CoffeeScript",
-	cpp = "C++",
-	csharp = "C#",
-	css = "CSS",
-	cuda = "CUDA",
-	fsharp = "F#",
-	glsl = "GLSL",
-	html = "HTML",
-	javascript = "JavaScript",
-	json = "JSON",
-	matlab = "MATLAB",
-	moonscript = "MoonScript",
-	objective_c = "Objective-C",
-	php = "PHP",
-	powershell = "PowerShell",
-	sql = "SQL",
-	toml = "TOML",
-	typescript = "TypeScript",
-	vb = "Visual Basic",
-	vhdl = "VHDL",
-	xml = "XML",
-	yaml = "YAML"
 }
 -- LuaFormatter on
 
@@ -110,14 +83,8 @@ local function discord_status(updated)
 end
 
 local function attach_handlers()
-	events.connect(events.QUIT, function()
-		M.rpc.close()
-		return nil
-	end, 1)
-	events.connect(events.RESET_BEFORE, function()
-		M.rpc.close();
-		is_connected = false
-	end)
+	events.connect(events.QUIT, M.close)
+	events.connect(events.RESET_BEFORE, M.close)
 	events.connect(events.SAVE_POINT_REACHED, M.update)
 	events.connect(events.SAVE_POINT_LEFT, M.update)
 	events.connect(events.BUFFER_AFTER_SWITCH, M.update)
@@ -125,24 +92,12 @@ local function attach_handlers()
 	events.connect(events.LEXER_LOADED, M.update)
 	events.connect(events.BUFFER_NEW, M.update)
 	if M.show_connected then events.connect(events.UPDATE_UI, discord_status) end
-
-	events.connect(events.BUILD_OUTPUT, function() last_action = 'building ' end)
-	events.connect(events.COMPILE_OUTPUT, function() last_action = 'compiling ' end)
-	events.connect(events.RUN_OUTPUT, function() last_action = 'running ' end)
-	events.connect(events.TEST_OUTPUT, function() last_action = 'testing ' end)
+	handlers = true
 end
 
 local function remove_handlers()
-	events.disconnect(events.QUIT, function()
-		M.rpc.close()
-		return nil
-	end, 1) -- TODO: Is this right?
-
-	events.disconnect(events.RESET_BEFORE, function()
-		M.rpc.close();
-		is_connected = false
-	end)
-
+	events.disconnect(events.QUIT, M.close)
+	events.disconnect(events.RESET_BEFORE, M.close)
 	events.disconnect(events.SAVE_POINT_REACHED, M.update)
 	events.disconnect(events.SAVE_POINT_LEFT, M.update)
 	events.disconnect(events.BUFFER_AFTER_SWITCH, M.update)
@@ -150,31 +105,38 @@ local function remove_handlers()
 	events.disconnect(events.LEXER_LOADED, M.update)
 	events.disconnect(events.BUFFER_NEW, M.update)
 	if M.show_connected then events.disconnect(events.UPDATE_UI, discord_status) end
+	handlers = false
 end
 
-local old_display_name = 'Untitled'  -- So we can track running/building
+-- Update details in M.presence
 local function update_presence_details()
 	local task = buffer.modify and 'editing ' or 'viewing '
-	local display_name = old_display_name
+	local display_name
+	local lexer_name
 
-	if (M.display_names[buffer:get_lexer()]) then
-		display_name = M.display_names[buffer:get_lexer()]
-	elseif buffer:get_lexer() ~= 'output' then
-		display_name = buffer:get_lexer():sub(1, 1):upper() .. buffer:get_lexer():sub(2)
-	else
+	if buffer:get_lexer() == 'output' then
+		lexer_name = old_lexer
 		task = last_action
+	else
+		lexer_name = buffer:get_lexer()
+		old_lexer = lexer_name
 	end
-	old_display_name = display_name
+
+	if M.display.names[lexer_name] then
+		display_name = M.display.names[lexer_name]
+	else -- Capitalise it
+		display_name = lexer_name:sub(1, 1):upper() .. lexer_name:sub(2)
+	end
 
 	local filestate = ' file.'
-	if M.private_mode then
-		-- TODO: 'an' would sometimes be more appropriate
-		filestate = 'a ' .. display_name .. (display_name:find('file') and '.' or filestate)
+	if M.private then
+		filestate = (M.display.an[lexer_name] and 'an ' or 'a ') .. display_name ..
+			(display_name:find('file') and '.' or filestate)
 	else
 		if buffer.filename then
 			local their = ''
-			if (buffer.filename:match('.textadept/init.lua') or
-				buffer.filename:match('.textadept\\init.lua')) then
+			if buffer.filename:match('.textadept/init.lua') or
+				buffer.filename:match('.textadept\\init.lua') then
 				their = 'their Textadept ' -- Call em out
 			end
 			filestate = their .. buffer.filename:match('[^/\\]+$')
@@ -184,10 +146,9 @@ local function update_presence_details()
 	if task == 'building ' or task == 'testing ' then filestate = 'a project.' end
 	M.presence.state = 'Currently ' .. task .. filestate
 
-	-- Details
 	-- IDEAS: Time since most recent commit? Git branch name?
 	M.presence.details = ''
-	if io.get_project_root() and not M.private_mode then
+	if io.get_project_root() and not M.private then
 		M.presence.details = 'Project directory: ' .. (io.get_project_root():match('[^/\\]+$'))
 	end
 
@@ -198,76 +159,71 @@ local function update_presence_details()
 	end
 
 	M.presence.largeImageKey = buffer:get_lexer()
-	M.presence.largeImageText = 'Working on a ' .. display_name ..
-		(display_name:find('file') and '.' or ' file.')
+	M.presence.largeImageText = 'Working on ' .. (M.display.an[lexer_name] and 'an ' or 'a ') ..
+		display_name .. (display_name:find('file') and '.' or ' file.')
 end
 
--- Convenience wrapper that will get current details before calling rpc.update() and update UI
+-- Get current details then update RPC and UI
 function M.update()
 	update_presence_details()
 	M.stats = M.rpc.update(M.presence)
 
-	if (M.stats.lastCallback == 0) then
+	if M.stats.lastCallback == 0 then
 		-- Discord hasn't run any handlers yet, try sending another update
 		is_connected = false
-		if (attempts ~= M.connect_attempts) then
+		if attempts ~= M.attempts then
 			attempts = attempts + 1
-			timeout(0.2, function()
+			timeout(0.3, function()
 				ui.statusbar_text = 'Attempting to connect to Discord...'
 				M.update()
 			end)
 		else
 			ui.statusbar_text = 'Could not connect to Discord.'
-			M.rpc.close() -- Just in case it's some weird connection issue
-			attempts = 0
+			M.close() -- Just in case it's some weird connection issue
 		end
 	elseif M.stats.lastCallback == 1 then
-		if (is_connected == false) then
+		if is_connected == false then
 			ui.statusbar_text = 'Discord: Connected to ' .. M.stats.globalName .. '.'
 			is_connected = true;
-			attach_handlers()
 		end
 	elseif M.stats.lastCallback == 2 then
 		ui.statusbar_text = 'Discord Disconnect: ' .. M.stats.errcode .. M.stats.errorDetails
-		is_connected = false; -- remove_handlers()
-		M.rpc.close()
+		is_connected = false; -- M.close()
 	elseif M.stats.lastCallback == 3 then
 		ui.statusbar_text = 'Discord Error: ' .. M.stats.errcode .. M.stats.errorDetails
-		is_connected = false; -- remove_handlers()
-		M.rpc.close()
+		is_connected = false; -- M.close()
 	end
 end
 
 function M.close()
-	attempts = 0
-	if is_connected then
-		is_connected = false
-		remove_handlers()
-	end
+	is_connected = false
+	if handlers then remove_handlers() end
 	M.rpc.close()
 end
 
--- Manually connect to Discord - not suitable for calling from init.lua
--- NOTE: The Discord RPC library has it's own retry mechanism
--- that might mean this won't run for a while if using to reconnect
+-- Connect to Discord - not suitable for calling from init.lua
 function M.connect()
-	M.close()
-	timeout(0.2, function()
-		M.rpc.init()
-		M.update()
-	end)
+	attempts = 0
+	M.rpc.init()
+	M.presence.startTimestamp = os.time()
+	M.update()
+	if not handlers then attach_handlers() end
 end
 
--- Convenience to allow user to 'start' RPC in their init.lua
--- Actually starts RPC once Textadept is fully initialised (so buffer/lexer names etc. will be present)
+-- Allow user to 'start' RPC in their init.lua
 M.mt.__call = function()
-	attempts = 0
-	events.connect(events.INITIALIZED, function() M.connect() end)
+	events.connect(events.INITIALIZED, M.connect)
 end
 M.mt.__metatable = 'Don\'t change Discord RPC Metatable'
 setmetatable(M, M.mt)
 
--- Add a menu entry.
+-- Just always connect these
+events.connect(events.BUILD_OUTPUT, function() last_action = 'building ' end)
+events.connect(events.COMPILE_OUTPUT, function() last_action = 'compiling ' end)
+events.connect(events.RUN_OUTPUT, function() last_action = 'running ' end)
+events.connect(events.TEST_OUTPUT, function() last_action = 'testing ' end)
+
+-- Menu entry.
 _L['Discord RPC'] = '_Discord RPC'
 _L['Connect/Reconnect'] = '_Connect/Reconnect'
 _L['Disconnect'] = '_Disconnect'
@@ -276,10 +232,17 @@ local discord_menu = {
 	title = _L['Discord RPC'], {_L['Connect/Reconnect'], M.connect}, {_L['Disconnect'], M.close}, {
 		_L['Status'], function()
 			ui.dialogs.message{
-				title = 'Discord RPC Status',
-				text = 'Username: ' .. M.stats.username .. '\n' .. 'Global Name: ' ..
-					M.stats.globalName .. '\n' .. 'User ID: ' .. M.stats.userId .. '\n' ..
-					'Connected: ' .. (is_connected and 'Yes' or 'No')
+				title = 'Discord RPC Status', text =
+				-- LuaFormatter off
+				'Username: ' .. M.stats.username .. '\n' ..
+				'Global Name: ' .. M.stats.globalName .. '\n' ..
+				'User ID: ' .. M.stats.userId .. '\n' ..
+				'Discriminator' .. M.stats.discriminator .. '\n' ..
+				'Last Callback: ' .. M.stats.lastCallback .. '\n' ..
+				'Error Code: ' .. M.stats.errcode .. '\n' ..
+				'Error Message: ' .. M.stats.errorDetails .. '\n' ..
+				'Connected: ' .. (is_connected and 'Yes' or 'No')
+				-- LuaFormatter on
 			}
 		end
 	}
